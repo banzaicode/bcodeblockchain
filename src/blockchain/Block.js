6510aeb0fcdbbdaf4ec05f7759a7085bdefa413e
Banzai Code <banzaicode@gmail.com>
2022-07-05 20:26:41 -0300

    added the function to create the hash of the blocks

--- src/blockchain/Block.js
+++ src/blockchain/Block.js
@@ -1,9 +1,16 @@
+import pkg from 'crypto-js';
+
 class Block {
   constructor(timestamp, prevHash, hash, data) {
     this.timestamp = timestamp;
     this.prevHash = prevHash;
     this.hash = hash;
     this.data = data;
+  }
+
+  static hash(timestamp, prevHash, data) {
+    const { SHA256 } = pkg;
+    return SHA256(`${timestamp}${prevHash}${data}`).toString();
   }
 
   toString() {
