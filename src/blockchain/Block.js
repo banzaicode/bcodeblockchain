75ad39da7f36dddb98dc724a144b0783962fdf7b
Banzai Code <banzaicode@gmail.com>
2022-07-05 20:45:37 -0300

    added function to create genesis block

--- src/blockchain/Block.js
+++ src/blockchain/Block.js
@@ -8,6 +8,13 @@ class Block {
     this.data = data;
   }
 
+  static get genesis() {
+    const timestamp = (new Date(2010, 0, 1)).getTime();
+    const data = 'genesis-data';
+    const hash = Block.hash(timestamp, undefined, data);
+    return new this(timestamp, undefined, hash, data);
+  }
+
   static hash(timestamp, prevHash, data) {
     const { SHA256 } = pkg;
     return SHA256(`${timestamp}${prevHash}${data}`).toString();
