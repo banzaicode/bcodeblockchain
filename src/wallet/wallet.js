2234eed4b0042e2a0197b346cf096e8a7c720700
RockBanzai <hrocca76@gmail.com>
2022-09-17 14:40:24 -0300

    feat: added get current wallet balance

--- src/wallet/wallet.js
+++ src/wallet/wallet.js
@@ -16,9 +16,9 @@ class Wallet {
     }
 
     createTransaction(receiverAddress, amount) {
-        const { balance, blockchain: { memoryPool } } = this;
+        const { currentBalance, blockchain: { memoryPool } } = this;
 
-        if (amount > balance) throw Error(`the amount is greater than the balance`)
+        if (amount > currentBalance) throw Error(`the amount is greater than the balance`)
         let transaction = memoryPool.find(this.publicKey);
         if (transaction) {
           transaction.update(this, receiverAddress, amount);
@@ -28,6 +28,38 @@ class Wallet {
         }
     
         return transaction;
+    }
+
+    get currentBalance() {
+        const { blockchain: { blocks = [] }, publicKey } = this;
+        let { balance } = this;
+        const transactions = [];
+
+        blocks.forEach(({ data = [] }) => {
+            if (Array.isArray(data)) data.forEach((tx) => transactions.push(tx));
+        });
+
+        const walletInputTransactions = transactions.filter((tx) => tx.input.address === publicKey);
+        let timestamp = 0;
+
+        if (walletInputTransactions.length > 0) {
+            const recentInputTransactions = walletInputTransactions
+                .sort((a, b) => a.input.timestamp - b.input.timestamp)
+                .pop();
+
+            balance = recentInputTransactions.outputs.find(({address}) => address === publicKey).amount;
+            timestamp = recentInputTransactions.input.timestamp;
+        }
+
+        transactions
+            .filter(({ input }) => input.timestamp > timestamp)
+            .forEach(({ outputs }) => {
+                outputs.find(({ address, amount }) => {
+                    if (address === publicKey) balance += amount;
+                });
+            });
+
+        return balance;
     }
 
     toString() {
